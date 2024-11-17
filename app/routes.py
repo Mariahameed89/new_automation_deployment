2ee6bf41358eae404cf5a502cb51412b2a594384
Mariahameed89 <134937624+Mariahameed89@users.noreply.github.com>
2024-11-17 20:33:17 +0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -41,7 +41,7 @@ def browser_init():
                     "http": proxy_url,
                     "https": proxy_url
                 }
-            
+            }
             # Set Chrome options
             chrome_options = Options()
             chrome_options.add_argument("--headless=new")  # Ensure headless mode is enabled
@@ -53,7 +53,7 @@ def browser_init():
             chrome_options.add_argument("--disable-blink-features=AutomationControlled")
             chrome_options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-            )
+            
 
 
             # Set up the WebDriver
